9f1e05d78087b248aa539337cefa68b8918b552f
Alan Matos <mr.alanmatos@hotmail.com>
2022-05-31 12:19:18 -0700

    updated files

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+const pokemonName = document.getElementById('pokemonList');
+const pokemonUrl = document.getElementById();
+
+/*
+
+const new = document.getElementById();
+const new = document.getElementById();
+const new = document.getElementById();
+const new = document.getElementById();
+
+
+*/
+
+
+fetch('https://pokeapi.co/api/v2/pokemon')
+.then( response => response.json())
+.then ( pokeData => {
+
+    console.log(pokeData)
+
+
+
+    for ( pokemon of pokeData ) {
+
+
+        const pokeList = document.createElement ('div');
+         document.getElementById();
+
+
+    } // end of foor loop function
+
+
+}) // end of .then function
