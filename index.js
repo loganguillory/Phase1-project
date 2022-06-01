c888710ea59a45cf713681edade84784e64b06e3
Alan Matos <mr.alanmatos@hotmail.com>
2022-05-31 17:57:34 -0700

    updates

--- index.js
+++ index.js
@@ -16,58 +16,58 @@ const new = document.getElementById();
 
 
 fetch('https://pokeapi.co/api/v2/pokemon')
-.then( response => response.json())
-.then ( pokeData => {
+    .then(response => response.json())
+    .then(pokeData => {
 
-    console.log(pokeData.results)
+        console.log(pokeData.results)
 
-    for ( const pokemon of pokeData.results ) {
+        for (const pokemon of pokeData.results) {
 
-        //console.log(pokemon.name)
+            //console.log(pokemon.name)
 
-        const pokeList = document.createElement('h4');
-        pokeList.textContent = pokemon.name;
-        pokeDisplay.append(pokeList)
-
-
-        pokeList.addEventListener('click', (e) => {
-            //pokemonShowcase(pokemon)
-
-            let pokeLink = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
-            console.log(pokeLink)
-            fetch(pokeLink)
-            .then (response => response.json())
-            .then ( newRequest => { 
-                console.log(newRequest)
-                pokemonShowcase(newRequest)
+            const pokeList = document.createElement('li');
+            pokeList.textContent = pokemon.name;
+            pokeDisplay.append(pokeList)
 
 
+            pokeList.addEventListener('click', (e) => {
+                //pokemonShowcase(pokemon)
+
+                let pokeLink = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
+                console.log(pokeLink)
+                fetch(pokeLink)
+                    .then(response => response.json())
+                    .then(newRequest => {
+                        console.log(newRequest)
+                        pokemonShowcase(newRequest)
 
 
 
 
 
-            });
-
-        }); // ends event listener 1
 
 
-    }; // ends for loop
+                    });
+
+            }); // ends event listener 1
 
 
-    function pokemonShowcase (poke) {
-
-        console.log(poke.sprites.back_default)
-        pokemonName.textContent = poke.name;
-        pokeicon.src = poke.sprites.front_default
-        //pokemonImg.src = 
-
-    }; // ends pokemonShowcase function 
+        }; // ends for loop
 
 
+        function pokemonShowcase(poke) {
+
+            console.log(poke.sprites.back_default)
+            pokemonName.textContent = poke.name;
+            pokeicon.src = poke.sprites.front_default
+            //pokemonImg.src = 
+
+        }; // ends pokemonShowcase function 
 
 
-}); // ends .then function
+
+
+    }); // ends .then function
 
 
 
