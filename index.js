6aaf2528a82b7eb6e9400ed7512f59760cb3ecf3
Alan Matos <mr.alanmatos@hotmail.com>
2022-05-31 17:55:55 -0700

    updates

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const pokeDisplay = document.getElementById('pokemon-info');
 const pokemonDetails = document.getElementById('pokemon-details');
 const pokeImg = document.getElementById('pokemon-image');
 const pokemonName = document.getElementById('pokemonName');
+const pokeicon = document.getElementById('pokemon-icon');
+//et pokeLink =
+
 /*
-
-
-
 
 const new = document.getElementById();
 const new = document.getElementById();
@@ -31,17 +31,23 @@ fetch('https://pokeapi.co/api/v2/pokemon')
 
 
         pokeList.addEventListener('click', (e) => {
-            pokemonShowcase(pokemon)
+            //pokemonShowcase(pokemon)
 
-            let pokeLink = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.name}.png`;
+            let pokeLink = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
             console.log(pokeLink)
-            //fetch('pokeLink')
-            //.then (response => response.json())
-            //.then ( newRequest => { 
-            //    console.log(newRequest)
+            fetch(pokeLink)
+            .then (response => response.json())
+            .then ( newRequest => { 
+                console.log(newRequest)
+                pokemonShowcase(newRequest)
 
 
-        //    })
+
+
+
+
+
+            });
 
         }); // ends event listener 1
 
@@ -51,14 +57,10 @@ fetch('https://pokeapi.co/api/v2/pokemon')
 
     function pokemonShowcase (poke) {
 
-
-        //console.log(poke)
+        console.log(poke.sprites.back_default)
         pokemonName.textContent = poke.name;
-        pokemonDescription.textContent = poke.url;
+        pokeicon.src = poke.sprites.front_default
         //pokemonImg.src = 
-        
-
-
 
     }; // ends pokemonShowcase function 
 
